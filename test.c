2263c25a182ea9be85648ce2f98deea680e6518a
HamzaGorcevic <gorcevichamza76@gmail.com>
2022-10-30 16:23:00 +0100

    exercise 4.33

--- test.c
+++ test.c
@@ -18,17 +18,17 @@ int main()
 
 // OVo nije htelo
 
-    int i,n;
-    float S = 1,p=0,clan;
+    // int i,n;
+    // float S = 1,p=0,clan;
 
-    printf("Unesite  ");
-    scanf("%d",&n);
-    for(i=1;i<=n;i++){
-        p=sqrt(2+p);
-        clan = 1/p;
-        S = S *clan;
-    };
-    printf("Dobijemo %.2f",S);
+    // printf("Unesite  ");
+    // scanf("%d",&n);
+    // for(i=1;i<=n;i++){
+    //     p=sqrt(2+p);
+    //     clan = 1/p;
+    //     S = S *clan;
+    // };
+    // printf("Dobijemo %.2f",S);
 
 
 
@@ -64,9 +64,128 @@ int main()
 // printf("dobilie ste ocenu  %f", ceil(1.0 * broj / 10)); 
 
 
-
+     int i, j, n;
+     int addon = 0;
+     printf(" n= ");
+     scanf("%d", &n);
+     printf("\n");
+     
+    //  for(i=0;i<n;i++){
+    //      if(i==0 || i == n-1){
+    //          printf("+");
+    //      }
+    //      else{
+    //          printf("-");
+    //      }
+         
+    
+    //  }
+    //  printf("\n");
+     
+    //  for(j=0;j<n-2;j++){
+    //      if(j == ceil((n-2)/2)){
+    //          for(i =0;i<n;i++){
+    //              if(i== ceil(n/2)){
+    //                 printf("+");
+    //             }else{
+    //                 printf("-");
+    //             }
+                
+    //          }
+    //          printf("\n");
+    //      }else{
+    //          for(i=0;i<n;i++){
+    //          if(i==0 || i == n-1 || i == ceil(n/2) ){
+    //          printf("|");
+    //          }
+    //          else{
+    //              printf(" ");
+    //          }
+             
+    //      }
+    //          printf("\n");
+             
+    //      }
+         
+         
+    //  }
+     
+    //  for(i=0;i<n;i++){
+    //      if(i==0 || i == n-1){
+    //          printf("+");
+    //      }
+    //      else{
+    //          printf("-");
+    //      }
+         
+    
+    //  }
+    
+    // for(i=0;i<n;i++){
+    //     if(i==0 || i==n-1){
+    //         printf("+");
+    //     }
+    //     else printf("-");
+    // }
+    // printf("\n");
     
 
+
+    // for(i=0;i<n-2;i++){
+    //     for(j=0;j<n;j++){
+    //         if(j==0){
+    //             addon++;
+    //             printf("|");
+    //         }
+    //         if(j==n-1){
+    //             printf("|");
+    //         }else if(addon == j) printf("/");else printf(" ");
+    //     }
+    //     printf("\n");
+    // }
+    
+    // for(i=0;i<n;i++){
+    //     if(i==0 || i==n-1){
+    //         printf("+");
+    //     }
+    //     else printf("-");
+    // }
+    
+    
+ // 4.33
+    
+         int n, i, j, nzv=0, kor=1;
+         char rep = ' ';
+         printf(" n= ");
+         scanf("%d", &n);
+         printf("\n");
+         
+        for(i=1;i<=n*3;i++){
+            nzv +=kor;
+            
+            for(j=1;j<=n*3;j++){
+                printf("%c",rep);
+            }
+            for(j=1;j<=nzv;j++){
+                printf("*");
+            }
+           printf("\n");
+           
+           
+           
+         
+           
+           
+            if(i==n){
+                rep = '*';
+            }
+            if(i==3*n/2+1){
+                kor=-1;
+            }
+            if(i==2*n) rep =' ';
+
+         
+        }
 
     return 0;
 }
