80b8c19c07357456465183b9e6b6a20ab2ef4265
HamzaGorcevic <gorcevichamza76@gmail.com>
2022-11-16 22:38:14 +0100

    zadatak 3

--- test.c
+++ test.c
@@ -1,303 +1,122 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
- 
-int main()
-{
-    // int broj ;
-    // float ocena;
-    // int i;
-    // printf('unesite broj');
-    // scanf("%d" ,&broj);
-    // ocena = 1.0;
-
-    // for(i=1;i<broj;i++){
-    //     ocena *=i;
-    // };
-    // printf("ovo je rezultat %f" , ocena);
-
-// OVo nije htelo
-
-    // int i,n;
-    // float S = 1,p=0,clan;
-
-    // printf("Unesite  ");
-    // scanf("%d",&n);
-    // for(i=1;i<=n;i++){
-    //     p=sqrt(2+p);
-    //     clan = 1/p;
-    //     S = S *clan;
-    // };
-    // printf("Dobijemo %.2f",S);
+// ZADACI ZA PRVI KOLOKVIJUM 11/17/2022;
 
 
 
-    // printf("Unesite neki borj\n");
-    // scanf("%d",&broj);
+/////////////////////////////////////////////////
 
-    // if(broj % 2 == 0){
-    //     printf("Broj je paran KKK");
-    // }
-    // else{
-    //     printf("Broj je neparan ");
-    // }
-
-    // if(broj < 0){
-    //     printf("Broj je manji od nule");
-    // }else if(broj > 0){
-    //     printf("Broj je veci od nule");
-    // }else{
-    //     printf("Broj je nula");
-    // }
-
-    // if(broj < broj1){
-    //     broj = broj1;
-        
-    // }
-    // if(broj < broj2){
-    //     broj = broj2;}
-
-
-    // printf("%d",broj);
-
-
-// printf("dobilie ste ocenu  %f", ceil(1.0 * broj / 10)); 
-
-
-    //  int i, j, n;
-    //  int addon = 0;
-    //  printf(" n= ");
-    //  scanf("%d", &n);
-    //  printf("\n");
-     
-    //  for(i=0;i<n;i++){
-    //      if(i==0 || i == n-1){
-    //          printf("+");
-    //      }
-    //      else{
-    //          printf("-");
-    //      }
-         
-    
-    //  }
-    //  printf("\n");
-     
-    //  for(j=0;j<n-2;j++){
-    //      if(j == ceil((n-2)/2)){
-    //          for(i =0;i<n;i++){
-    //              if(i== ceil(n/2)){
-    //                 printf("+");
-    //             }else{
-    //                 printf("-");
-    //             }
-                
-    //          }
-    //          printf("\n");
-    //      }else{
-    //          for(i=0;i<n;i++){
-    //          if(i==0 || i == n-1 || i == ceil(n/2) ){
-    //          printf("|");
-    //          }
-    //          else{
-    //              printf(" ");
-    //          }
-             
-    //      }
-    //          printf("\n");
-             
-    //      }
-         
-         
-    //  }
-     
-    //  for(i=0;i<n;i++){
-    //      if(i==0 || i == n-1){
-    //          printf("+");
-    //      }
-    //      else{
-    //          printf("-");
-    //      }
-         
-    
-    //  }
-    
-    // for(i=0;i<n;i++){
-    //     if(i==0 || i==n-1){
-    //         printf("+");
-    //     }
-    //     else printf("-");
-    // }
-    // printf("\n");
-    
-
-
-    // for(i=0;i<n-2;i++){
-    //     for(j=0;j<n;j++){
-    //         if(j==0){
-    //             addon++;
-    //             printf("|");
-    //         }
-    //         if(j==n-1){
-    //             printf("|");
-    //         }else if(addon == j) printf("/");else printf(" ");
-    //     }
-    //     printf("\n");
-    // }
-    
-    // for(i=0;i<n;i++){
-    //     if(i==0 || i==n-1){
-    //         printf("+");
-    //     }
-    //     else printf("-");
-    // }
-    
-    
- // 4.33
-    
-        //  int n, i, j, nzv=0, kor=1;
-        //  char rep = ' ';
-        //  printf(" n= ");
-        //  scanf("%d", &n);
-        //  printf("\n");
-         
-        // for(i=1;i<=n*3;i++){
-        //     nzv +=kor;
-            
-        //     for(j=1;j<=n*3;j++){
-        //         printf("%c",rep);
-        //     }
-        //     for(j=1;j<=nzv;j++){
-        //         printf("*");
-        //     }
-        //    printf("\n");
-           
-           
-           
-         
-           
-           
-        //     if(i==n){
-        //         rep = '*';
-        //     }
-        //     if(i==3*n/2+1){
-        //         kor=-1;
-        //     }
-        //     if(i==2*n) rep =' ';
-
-         
-        // }
-
-        // zadatak 4.37
-
-        //         int n,i,j;
-        // do{
-        //     printf("unesite broj ");
-        //     scanf("%d",&n);
-        // }
-        // while(n%2 == 0);
-        // printf("+");
-        // for(i=0;i<n-2;i++){
-        //     printf("-");
-        // }
-        // printf("+\n");
-        
-        // for(i=1;i<n-1;i++){
-              
-        //     printf("|");
-        //    for(j=1;j<n-1;j++){
-        //     if(j == n/2 && i==n/2){
-        //         printf("X");
-              
-        //     }else if(j==i){
-        //         printf("\\");
-        //     }else if(j==n-1-i){
-        //         printf("/");
-        //     }else printf(" ");
-        //    }
-        //     printf("|\n");
-            
-        // }
-        
-        // printf("+");
-        // for(i=0;i<n-2;i++){
-        //     printf("-");
-        // }
-        // printf("+");
-
-        // zadatak sa casa 
-
-        // int n,a,b,c;
-        // printf("unesite broj \n");
-        
-        // scanf("%d",&n);
-        
-        // if(n > 99 && n <999){
-        //     a = n / 100;
-        //     b = n % 100 / 10;
-        //     c = n % 10;
-            
-        // if(a > b && a>c && a!=0){
-        //     printf("ovo je resenje %d%d%d \n",b,a,c);
-        //     printf("ovo je resenje %d%d%d",c,a,b);
-        // }else if(b > a && b>c && b!=0){
-        //     printf("ovo je resenje %d%d%d \n",a,b,c);
-        //     printf("ovo je resenje %d%d%d",c,b,a);
-        // }else if(c > a && c>b && b!=0){
-        //     printf("ovo je resenje %d%d%d \n",a,c,b);
-        //     printf("ovo je resenje %d%d%d",b,c,a);
-        // }
-            
-        // }else{
-        //     printf("drugi borj");
-        //     scanf("%d",&n);
-        // }
-
-// exercise 7
-//  int i, x=0;
-//  for(i=1; i<20; i++)
-//  {
-//  if(i%5==0) continue;
-//  x++;
-//  if(i==10) break;
-//  printf("\n x=%d", x);
-//  }
-//   return 10;
-    
-
-        
-        
-
-
-
-//     return 0;
-// }
-
-
-
-// ZADATAk za kolkvijum
+// 1) Proveriti da li je uneti broj deljiv sa faktorijelom svih svojih cifara
 // #include <stdio.h>
 
 // int main()
 // {
-//     int n, counter = 0;
-    
+//     int n,p,pomocna,i;
+//     printf("unesite vrednost:");
 //     scanf("%d",&n);
-//     int m = n;
-//     while(m != 0){
-//          m /= 10;
-//         counter ++;
-         
-//     }
-//     int cifre = pow(10,(counter - 1));
-//     int prvi = n % 10;
-//     int poslednji = n / cifre;
-//     int srednja = n - poslednji * cifre - prvi + poslednji;
-//     printf(" konacno %d%d",prvi,srednja);
+//     pomocna = n;
+//     while(pomocna > 0){
+//         p = pomocna % 10;
+//         pomocna /=10;
+//         int faktori = 1;
+//         for(i=1; i<=p; i++){
+//             faktori *=i;
+//         }
+//         if(n % faktori == 0){
+//             printf("deljiv je sa %d \n",faktori);
+//         }else{
+//             printf("nije deljiv sa %d \n",faktori);
+//         }
+            
+//         }
+        
     
-    
-    
-    
-
 
 //     return 0;
 // }
+
+/////////////////////////////////////////////////
+
+
+
+/////////////////////////////////////////////////
+
+// 2) odrediti da li se tacka nalazi u trouglu na kordinatnom sistemu
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int x,y;
+    
+//     printf("Unesite tacku x");
+//     scanf("%d",&x);
+//     printf("Unesite tacku y");
+//     scanf("%d",&y);
+    
+//     // tacke ivica trougla
+//     int x1 = -1;
+//     int y1 = 1;
+    
+//     int x2 = 3;
+//     int y2 = 1;
+    
+//     int x3 = 2;
+//     int y3 = 3;
+    
+    
+    
+//     int p = abs(x1*(y2 - y3)+x2*(y3-y1)+x3*(y1-y2))/2;
+    
+//     int p1 =abs(x*(y2-y3) + x2*(y3-y) + x3*(y-y2)) /2;
+//     int p2 = abs(x1*(y-y3) + x*(y3-y1) + x3*(y1-y2)) /2;
+//     int p3 = abs(x1*(y2-y) + x2*(y3-y1) + x*(y1-y)) /2;
+    
+//     if(p == (p1+p2+p3)){
+//         printf("u trouglu je");
+//     }else{
+//         printf("nije u trouglu");
+//     }
+    
+    
+
+//     return 0;
+// }
+
+
+
+
+
+/////////////////////////////////////////////////
+
+// 3) Pronaci sumu elementa sa n brojem cifara i sa e elementima , do p indeksa cife 
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i,n,e,p;
+//     int suma = 0;
+//     int sumaOstatka = 0;
+//     printf("unesi broj elemenata niza: \n");
+//     scanf("%d",&n);
+//     printf("unesite do kog broja sumirati \n");
+//     scanf("%d",&p);
+    
+//     for(i = 1;i<=n ;i++){
+//         printf("unesi element broj %d \n",i);
+//         scanf("%d",&e);
+//         if(i < p){
+//             suma+=e;
+//         }else{
+//             sumaOstatka+=e;
+            
+//         }
+//     }
+    
+    
+//     printf("suma %d \n",suma);
+//     printf(" suma ostatka %d \n",sumaOstatka);
+
+//     return 0;
+// }
+
+/////////////////////////////////////////////
