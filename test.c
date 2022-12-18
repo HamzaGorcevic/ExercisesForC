cedc3efe3c752bd49eadd1c068b09276956160d3
HamzaGorcevic <gorcevichamza76@gmail.com>
2022-12-19 00:02:28 +0100

    added 32 to check

--- test.c
+++ test.c
@@ -1769,3 +1769,74 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 //  return 0;
 // }
+
+///////////////////////////////////////////////////////////////////////
+
+//32)
+// #include <stdio.h>
+// #include <stdlib.h>
+// main()
+// {
+//  int *s1, *s2, *s3;
+//  int n1, n2, n3, i, j;
+//  printf ("\n Broj elemenata niza S1: ");
+//  scanf ("%d", &n1);
+//  s1=malloc(n1*sizeof(int));
+//  printf(" Elemenata niza S1= ");
+//  for(i=0; i<n1; i++)
+//  scanf("%d", &s1[i]);
+//  if(n1==0) printf("\n");
+//  printf("\n Broj elemenata niza S2= ");
+//  scanf("%d", &n2);
+//  s2=malloc(n2*sizeof(int));
+//  printf(" Elementi niza S2: ");
+//  for(i=0; i<n2; i++)
+//  scanf("%d", &s2[i]);
+//  if(n2==0) printf ("\n");
+//  /*Unija dva skupa*/
+//  s3=malloc((n1+n2)*sizeof(int));
+//  for(n3=0; n3<n1; n3++)
+//  s3[n3] = s1[n3];
+//  for(j=0; j<n2; j++)
+//  {
+//  for(i=0; i<n1 && s1[i]!=s2[j]; i++);
+//  if(i == n1)
+//  s3[n3++] = s2[j];
+//  }
+//  s3=realloc(s3, n3*sizeof(int));
+//  printf("\n S1+S2= ");
+//  for(i=0; i<n3; i++)
+//  printf("%d ", s3[i]);
+//  free(s3);
+//  /*Presek dva skupa*/
+//  s3=malloc((n1<n2 ? n1 : n2)*sizeof(int));
+//  for(n3=i=0; i<n1; i++)
+//  {
+//  for(j=0; j<n2 && s1[i]!=s2[j]; j++);
+//  if(j < n2)
+//  s3[n3++] = s1[i];
+//  }
+//  s3=realloc(s3, n3*sizeof(int));
+//  printf("\n S1*S2= ");
+//  for(i=0; i<n3; i++)
+//  printf("%d ", s3[i]);
+//  free(s3);
+//  /*Razlika dva skupa*/
+//  s3=malloc(n1*sizeof(int));
+//  for(n3=i=0; i<n1; i++)
+//  {
+//  for(j=0; j<n2 && s1[i]!=s2[j]; j++);
+//  if(j == n2)
+//  s3[n3++] = s1[i];
+//  }
+//  s3=realloc(s3, n3*sizeof(int));
+//  printf ("\n S1-S2= ");
+//  for(i=0; i<n3; i++)
+//  printf("%d ", s3[i]);
+//  free(s3);
+
+//  free(s1);
+//  free(s2);
+
+//  return 0;
+// } 
